61c49282d176b4d31c8947747d3d3f5a458853fe
prashuusrini <sps678456@gmail.com>
2025-09-25 14:36:41 +0530

    Delete model directory

--- model/app.py
+++
@@ -1,8 +0,0 @@
-import joblib
-
-model = joblib.load("model.pkl")
-
-def predict_drugs(symptom_input: str):
-    preds = model.predict([symptom_input])[0]
-    drug_labels = ['Paracetamol', 'Atenolol', 'Metformin']  # Map to real drug names
-    return [drug for drug, p in zip(drug_labels, preds) if p == 1]
